092e6c03275a8d4e6e93688cffac63b05c4ee64d
hsiam261 <hsiam261@gmail.com>
2025-10-01 20:38:04 +0600

    initial commit

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "A Mutating Webhook to Inject Deterministic Affinity into Statefulset Pods";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-stable }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages."${system}";
+    stable = nixpkgs-stable.legacyPackages."${system}";
+  in
+  {
+    devShells."${system}".default = pkgs.mkShell {
+    packages = [
+        stable.go
+        stable.gopls
+        stable.go-tools
+        stable.kubernetes-helm
+      ];
+    };
+
+    packages."${system}" = {
+      default = pkgs.buildGoModule rec {
+        pname="k8s-podlister";
+        version="0.1.0";
+        src=./src;
+        vendorHash=null;
+
+        env.CGO_ENABLED = "0";
+
+        ldflags = [
+          "-s"
+          "-w"
+        ];
+
+        nativeBuildInputs = [ stable.upx ];
+        postInstall = ''
+          # compress the binary after build
+          upx --best $out/bin/k8s-podlister-go
+        '';
+      };
+    };
+  };
+}
