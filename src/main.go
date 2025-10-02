7ca541b2e0807b09812259e1259188b54dde5adb
hsiam261 <hsiam261@gmail.com>
2025-10-03 00:08:37 +0600

    add webhook endpoint

--- src/main.go
+++ src/main.go
@@ -12,8 +12,8 @@ import (
 	"os/signal"
 	"syscall"
 
-	// corev1 "k8s.io/api/core/v1"
-	// admissionv1 "k8s.io/api/admission/v1"
+	corev1 "k8s.io/api/core/v1"
+	admissionv1 "k8s.io/api/admission/v1"
 )
 
 /*
@@ -22,69 +22,73 @@ import (
 	the stateful set annotation and from the pod name
 	decides what affinity should be injected to the pod
 */
-// func handleWebhook(w http.ResponseWriter, r *http.Request) {
-// 	var admissionReview admissionV1.AdmissionReview
+func handleWebhook(w http.ResponseWriter, r *http.Request) {
+	var admissionReview admissionV1.AdmissionReview
 
-// 	if err := json.NewDecoder(r.Body).Decode(&admissionReview); err != nil {
-// 		log.Printf("Could not decode request: %v", err)
-// 		http.Error(w, err.Error(), http.StatusBadRequest)
-// 		return
-// 	}
+	if err := json.NewDecoder(r.Body).Decode(&admissionReview); err != nil {
+		log.Printf("Could not decode request: %v", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
-// 	admissionRequest := admissionReview.Request
-// 	admissionResponse := admissionv1.AdmissionResponse{}
+	admissionRequest := admissionReview.Request
+	log.Printf("Request ID: %v", admissionReview.UID)
 
-// 	if admissionRequest.Resource != "pods" {
-// 		log.Printf("Admission request object should be a pod, but instead we got a %s", admissionRequest.Resource)
-// 		//NEED TO RETURN SOMETHING HERE
-// 	}
+	if admissionRequest.Resource != "pods" {
+		log.Printf("Admission request object should be a pod, but instead we got a %s", admissionRequest.Resource)
+		//NEED TO RETURN SOMETHING HERE
+	}
 
-// 	var pod corev1.Pod
-// 	if err := json.Unmarshal(admissionRequest.Object.Raw, &pod); err != nil {
-// 		log.Printf("Failed to parse pod object from request: %v", err)
-// 		http.Error(w, err.Error(), http.StatusInternalServerError)
-// 		return
-// 	}
-
-// 	statefulsetReferenceCount := 0
-// 	for _ , owner := range pod.OwnerReferences {
-// 		if owner.Kind == "StatefulSet" {
-// 			statefulsetReferenceCount = statefulsetReferenceCount + 1
-// 		}
-// 	}
-
-// 	if statefulsetReferenceCount == 0 {
-// 		log.Printf("Pod %s in namespace %s is not owned by a statefulset", pod.Name, pod.Namespace)
-// 		err := fmt.Errorf("Pod %s in namespace %s is owned by multiple statefulsets", pod.Name, pod.Namespace)
-// 		http.Error(w, err.Error(), http.StatusBadRequest)
-// 		return
-// 	}
-
-// 	if statefulsetReferenceCount > 1 {
-// 		log.Printf("Pod %s in namespace %s is owned by multiple statefulsets", pod.Name, pod.Namespace)
-// 		err := fmt.Errorf("Pod %s in namespace %s is owned by multiple statefulsets", pod.Name, pod.Namespace)
-// 		http.Error(w, err.Error(), http.StatusBadRequest)
-// 		return
-// 	}
-
-// 	annotations := pod.Annotations
-// 	affinity := pod.Spec.Affinity
-
-
-// 	for annotation, val := range annotations {
-// 		if annotation == "statefulset-node-affinity-injector.hsiam261.github.io/config" {
-// 			var affinityMap map[string]string
-// 			if err := json.Unmarshal([]byte(val), &affinityMap); err != nil {
-// 				log.Printf("Malformed annotation value in pod %s in namespace %s: %s", pod.Name, pod.Namespace, val)
-// 				http.Error(w, err.Error(), http.StatusBadRequest)
-// 				return
-// 			}
+	http.Error(w, fmt.Sprintf("Admission request object should be a pod, but instead we got a %s", admissionRequest.Resource), http.StatusBadRequest)
 
 
 
-// 		}
-// 	}
-// }
+	// var pod corev1.Pod
+	// if err := json.Unmarshal(admissionRequest.Object.Raw, &pod); err != nil {
+	// 	log.Printf("Failed to parse pod object from request: %v", err)
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
+
+	// statefulsetReferenceCount := 0
+	// for _ , owner := range pod.OwnerReferences {
+	// 	if owner.Kind == "StatefulSet" {
+	// 		statefulsetReferenceCount = statefulsetReferenceCount + 1
+	// 	}
+	// }
+
+	// if statefulsetReferenceCount == 0 {
+	// 	log.Printf("Pod %s in namespace %s is not owned by a statefulset", pod.Name, pod.Namespace)
+	// 	err := fmt.Errorf("Pod %s in namespace %s is owned by multiple statefulsets", pod.Name, pod.Namespace)
+	// 	http.Error(w, err.Error(), http.StatusBadRequest)
+	// 	return
+	// }
+
+	// if statefulsetReferenceCount > 1 {
+	// 	log.Printf("Pod %s in namespace %s is owned by multiple statefulsets", pod.Name, pod.Namespace)
+	// 	err := fmt.Errorf("Pod %s in namespace %s is owned by multiple statefulsets", pod.Name, pod.Namespace)
+	// 	http.Error(w, err.Error(), http.StatusBadRequest)
+	// 	return
+	// }
+
+	// annotations := pod.Annotations
+	// affinity := pod.Spec.Affinity
+
+
+	// for annotation, val := range annotations {
+	// 	if annotation == "statefulset-node-affinity-injector.hsiam261.github.io/config" {
+	// 		var affinityMap map[string]string
+	// 		if err := json.Unmarshal([]byte(val), &affinityMap); err != nil {
+	// 			log.Printf("Malformed annotation value in pod %s in namespace %s: %s", pod.Name, pod.Namespace, val)
+	// 			http.Error(w, err.Error(), http.StatusBadRequest)
+	// 			return
+	// 		}
+
+
+
+	// 	}
+	// }
+}
 
 type ServerOptions struct {
 	EnableTLS bool
@@ -104,7 +108,7 @@ func handleStatus(w http.ResponseWriter, r *http.Request) {
 func runServer(serverOptions *ServerOptions) {
 	mux := http.NewServeMux()
 
-	// mux.HandleFunc("POST /webhook")
+	mux.HandleFunc("POST /webhook", handleWebhook)
 	mux.HandleFunc("/status", handleStatus)
 
 	port := 8080
